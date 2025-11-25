df89e492099bb7b460ef55352c52b4ef9bf44e33
timon162 <duyle8889@gmail.com>
2025-11-25 20:05:47 +0700

    Update routes/web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [AuthController::class, 'viewRegister'])->name('AuthController.viewRegister');
+Route::get('/login', [AuthController::class, 'viewLogin'])->name('AuthController.viewLogin');
+
+Route::post('/post-register', [AuthController::class, 'postRegister']);
