7fac879086f53c8828047d4ce8f3222acc60b782
timon162 <duyle8889@gmail.com>
2025-11-27 13:42:23 +0700

    add RememberTokenRequest

---
+++ app/Http/Requests/RememberTokenRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RememberTokenRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'remember_token' => 'required|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'remember_token.string'    => 'remember_token must be a string!',
+        ];
+    }
+}
