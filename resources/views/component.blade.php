b4c41777311254925274a2e636bf7907754a36ec
timon162 <duyle8889@gmail.com>
2025-11-27 13:49:37 +0700

    add component view

---
+++ resources/views/component.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>component</h1>
+</body>
+<script src="{{ asset('js/components/component.js') }}"></script>
+
+</html>
