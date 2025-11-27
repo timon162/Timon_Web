003d9ea6e72f8dfd4e9a17177c76310897a980de
timon162 <duyle8889@gmail.com>
2025-11-27 13:40:09 +0700

    Update AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RememberTokenRequest;
+use App\Http\Requests\LogoutRequest;
 use App\Services\Interfaces\AuthInterfaceService;
 
 class AuthController extends Controller
@@ -23,9 +26,31 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
+    public function viewUser()
+    {
+        return view('user.user_view');
+    }
+
     public function postRegister(RegisterRequest $request): JsonResponse
     {
         $response  = $this->authService->register($request->validated());
         return response()->json($response);
+    }
+
+    public function postLogin(LoginRequest $request): JsonResponse
+    {
+        $response  = $this->authService->login($request->validated());
+        return response()->json($response);
+    }
+
+    public function rememberMe(RememberTokenRequest $request): JsonResponse
+    {
+        $response  = $this->authService->rememberMe($request->validated());
+        return response()->json($response);
+    }
+
+    public function postLogout(LogoutRequest $request)
+    {
+        $this->authService->logout($request->validated());
     }
 }
