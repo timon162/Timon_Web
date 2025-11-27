a02af14725424e3bdca99262b74e154dd276ef7d
timon162 <duyle8889@gmail.com>
2025-11-27 13:44:29 +0700

    Update AuthRepository

--- app/Repositories/AuthRepository.php
+++ app/Repositories/AuthRepository.php
@@ -3,12 +3,43 @@
 namespace App\Repositories;
 
 use App\Repositories\Interfaces\AuthInterfaceRepository;
-use App\Models\WebUserModel;
+use App\Models\WebUser;
 
 class AuthRepository implements AuthInterfaceRepository
 {
-    public function register(array $data): WebUserModel
+    public function register(array $data): WebUser
     {
-        return WebUserModel::create($data);
+        return WebUser::create($data);
+    }
+
+    public function rememberToken(array $data): ?WebUser
+    {
+        $user = WebUser::where('email', $data['email'])->first();
+
+        if (!$user) {
+            return null;
+        }
+
+        $user->remember_token = $data['token'];
+        $user->save();
+
+        return $user;
+    }
+
+    public function rememberMe(array $data): ?WebUser
+    {
+        return WebUser::where('remember_token', $data['remember_token'])->first();
+    }
+
+    public function logout(array $data)
+    {
+        $user = WebUser::where('remember_token', $data['logout_token'])->first();
+
+        if (!$user) {
+            return null;
+        }
+
+        $user->remember_token = null;
+        $user->save();
     }
 }
