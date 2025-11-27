9d5bfd03a797c5c47325e7f904b685e72b435568
timon162 <duyle8889@gmail.com>
2025-11-27 13:45:15 +0700

    Update AuthInterfaceService

--- app/Services/Interfaces/AuthInterfaceService.php
+++ app/Services/Interfaces/AuthInterfaceService.php
@@ -5,4 +5,10 @@ namespace App\Services\Interfaces;
 interface AuthInterfaceService
 {
     public function register(array $data): array;
+
+    public function login(array $data): array;
+
+    public function rememberMe(array $data): array;
+
+    public function logout(array $data);
 }
