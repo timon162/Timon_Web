581cadb64fbf18537ce6fb7337c2f20210fe464e
timon162 <duyle8889@gmail.com>
2025-11-30 09:54:27 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', [AuthController::class, 'viewComponent'])->name('AuthController.viewComponent');
 
@@ -11,8 +12,11 @@ Route::post('/', [AuthController::class, 'rememberMe']);
 Route::get('/register', [AuthController::class, 'viewRegister'])->name('AuthController.viewRegister');
 Route::get('/login', [AuthController::class, 'viewLogin'])->name('AuthController.viewLogin');
 
-Route::get('/user-view', [AuthController::class, 'viewUser'])->name('AuthController.viewUser');
+Route::get('/user-view', [ProductController::class, 'viewUser'])->name('AuthController.viewUser');
 
 Route::post('/post-register', [AuthController::class, 'postRegister']);
 Route::post('/post-login', [AuthController::class, 'postLogin']);
 Route::post('/post-logout', [AuthController::class, 'postLogout']);
+
+Route::get('/product', [ProductController::class, 'getProduct']);
+Route::post('/post-product', [ProductController::class, 'postProduct']);
